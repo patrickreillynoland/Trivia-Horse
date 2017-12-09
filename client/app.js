34a2b0524c26279b0092bf8e71b65f35bee17f81
patrickreillynoland <patrickreillynoland@gmail.com>
2017-12-08 21:24:22 -0800

    Initial Commit

---
+++ client/app.js
@@ -0,0 +1,6 @@
+angular.module("Triviahorse", ['ngRoute', 'ngResource', 'ngAnimate', 'Triviahorse.controllers'])
+.config['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+    .when()
+}]
